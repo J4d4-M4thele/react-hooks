a709efd44051a2c6bd51a54a1a49abfa160dd4a3
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-01 09:47:03 +0200

    update

--- chapter06/src/App.js
+++ chapter06/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import UserForm from "./UserForm";
 
 function App() {
   return (
     <div>
-      
+      <UserForm />
     </div>
   );
 }
