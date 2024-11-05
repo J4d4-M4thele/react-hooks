6d2ac00cfb11519990c65fed455ef785f9c72c2a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-05 12:28:55 +0200

    update

--- chapter08/hooks-todos/src/App.js
+++ chapter08/hooks-todos/src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useReducer } from "react";
+import ToDoList from "./ToDoList";
+import { v4 as uuidv4 } from 'uuid';
+// import { UserContext } from "./index";
+// import { Button } from "react-bootstrap";
+
+const todosinitialState = {
+  todos: [
+    { id: 1, text: "finishing writing hooks chapter" },
+    { id: 2, text: "play with kids" },
+    { id: 3, text: "read bible" }
+  ]
+};
+
+function todosReducer(state, action) {
+  switch (action.type) {
+    /*****************CREATING***********************/
+    case 'add':
+      //generates unique id 
+      const newToDo = { id: uuidv4(), text: action.payload }
+      // add new todo onto array
+      const addedToDos = [...state.todos, newToDo]
+      // spread our state and assign todos
+      return { ...state, todos: addedToDos }
+      /*****************UPDATING***********************/
+    case 'edit':
+      const updatedToDo = { ...action.payload }
+      //find the todo by id
+      const updatedToDoIndex = state.todos.findIndex(t => t.id ===
+        action.payload.id)
+        //creates new array with the changed todo in between
+        //update todos text
+      const updatedToDos = [
+        ...state.todos.slice(0, updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ];
+      return { ...state, todos: updatedToDos }
+      /*****************DELETING***********************/
+    case 'delete':
+      //filters through array removing object we choose to delete
+      const filteredTodoState = state.todos.filter(todo => todo.id !==
+        action.payload.id)
+      return { ...state, todos: filteredTodoState }
+    default:
+      return todosinitialState
+  }
+}
+
+export const TodosContext = createContext();
 
 function App() {
+  const [state, dispatch] = useReducer(todosReducer, todosinitialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <ToDoList />
+    </TodosContext.Provider>
   );
 }
+
+//setting initial application state
+// const initialState = {
+//   count: 0
+// };
+
+// function reducer(state, action) {
+//   //different action types update the application
+//   //state in different ways
+//   switch (action.type) {
+//     case "increment":
+//       return { count: state.count + 1 }
+//     case "decrement":
+//       return { count: state.count - 1 }
+//     case "reset":
+//       return initialState
+//     default:
+//       return initialState
+//   }
+// };
+
+// function App() {
+//   //USECONTEXT HOOK
+//   //value recieved from index.js UserContext
+//   // const value = useContext(UserContext);
+
+//   //USEREDUCER HOOK
+//   //dispatch calls reducer function methods
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     // <div>
+//     //   Recieved, {value}
+//     // </div>
+
+//     <div>
+//       Count: {state.count}
+//       <br />
+//       <Button onClick={() => dispatch({ type: 'increment' })}>
+//         Increment
+//       </Button>
+//       <Button variant="secondary" onClick={() => dispatch({
+//         type:
+//           'decrement'
+//       })}>
+//         Decrement
+//       </Button>
+//       <Button variant="success" onClick={() => dispatch({
+//         type:
+//           'reset'
+//       })}>
+//         Reset
+//       </Button>
+//     </div>
+//   );
+// }
 
 export default App;
--- chapter08/hooks-todos/src/index.js
+++ chapter08/hooks-todos/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//context: allows us to share values to any components in the
+//component tree that asks for those values
+//made available to all components
+// export const UserContext = createContext();
+// const username = "Jada";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <UserContext.Provider value={username}> */}
     <App />
+    {/* </UserContext.Provider> */}
   </React.StrictMode>
 );
 
